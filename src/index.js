2dd79eec73a2f1f2feffee3646b4205300e43916
C <gypsy.sy@gmail.com>
2022-06-27 05:09:53 +0700

    fix update logo and update schema

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 'use strict';
 
+function stream2buffer(stream) {
+  return new Promise((resolve, reject) => {
+    const _buf = [];
+
+    stream.on('data', (chunk) => _buf.push(chunk));
+    stream.on('end', () => resolve(Buffer.concat(_buf)));
+    stream.on('error', (err) => reject(err));
+  });
+}
+
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -16,5 +26,14 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+        /**
+     * Temporary workaround for strapi-provider-upload-azure-storage
+     */
+         strapi.plugins.upload.provider.uploadStream = async (file) => {
+          file.buffer = await stream2buffer(file.stream);
+    
+          return strapi.plugins.upload.provider.upload(file);
+        };
+  },
 };
